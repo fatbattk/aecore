67947c1f926c95efe915247a33a1c04cc57e4e13
Anthony <anthony@aecore.com>
2015-03-31 20:05:46 -0700

    Setup a basic landing page.

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -4,9 +4,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>Aecore</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('/css/app-custom.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,20 +1,14 @@
 <html>
 	<head>
-		<title>Laravel</title>
 		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
+    <title>Aecore</title>
+    
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+		<link href="{{ asset('/css/app-custom.css') }}" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    
 		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+		
 
 			.container {
 				text-align: center;
@@ -30,18 +24,23 @@
 			.title {
 				font-size: 96px;
 				margin-bottom: 40px;
+        color:#3D464D;
+        text-shadow:1px 1px #FFF;
 			}
 
 			.quote {
-				font-size: 24px;
+				font-size: 30px;
+        color:#5D676D;
 			}
 		</style>
 	</head>
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+        <span class="logo-lg"></span>
+        <div class="quote">Construction software built by the Assembly community.<br>Coming Soon!</div>
+        <br><br>
+        <a class="btn btn-lg btn-success" href="http://www.assembly.com/aecore" target="_blank"><span class="glyphicon glyphicon-heart"></span> Get Involved</a>
 			</div>
 		</div>
 	</body>
